8a22f36172aa60eca9a24338a297032e09c40897
pavanis333 <sankeerthbussa@gmail.com>
2021-06-07 00:05:27 +0530

    Update ec2.py

--- ini/ec2.py
+++ ini/ec2.py
@@ -168,9 +168,9 @@ from boto import elasticache
 from boto import route53
 from boto import sts
 
-#from ansible.module_utils import six
+from ansible.module_utils import six
 #from ansible.module_utils import ec2 as ec2_utils
-#from ansible.module_utils.six.moves import configparser
+from ansible.module_utils.six.moves import configparser
 
 HAS_BOTO3 = False
 try:
